c47b5c24f56af72a0a7eef23542787189b0b3b19
leslieshea <lshea@audigy.com>
2023-04-19 15:39:08 -0700

    Able to grab random meme url w/ each button click

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import memesData from '../memesData';
+
+const data = memesData.data.memes;
+const meme =data[Math.floor(Math.random() * data.length)];
+console.log(meme.url);
 
 export default function Meme() {
+	function getMemeImage() {
+		const memesArray = memesData.data.memes;
+		const randomNumber = data[Math.floor(Math.random() * data.length)];
+		const url = memesArray[randomNumber].url;
+
+		console.log(url);
+	}
+
 	return (
 		<main>
-			<form className='form'>
+			<div className='form'>
 				<input
 					type='text'
 					className='form--input'
@@ -14,8 +27,13 @@ export default function Meme() {
 					className='form--input'
 					placeholder='Bottom text'
 				/>
-				<button className='form--button'>Get a new meme image 🖼️</button>
-			</form>
+				<button
+					className='form--button'
+					onClick={getMemeImage}
+				>
+					Get a new meme image 🖼️
+				</button>
+			</div>
 		</main>
 	)
 }
---
+++ src/memesData.js
@@ -0,0 +1,64 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+	"success": true,
+	"data": {
+		"memes": [
+			{
+				"id": "181913649",
+				"name": "Drake Hotline Bling",
+				"url": "https://i.imgflip.com/30b1gx.jpg",
+				"width": 1200,
+				"height": 1200,
+				"box_count": 2
+			},
+			{
+				"id": "87743020",
+				"name": "Two Buttons",
+				"url": "https://i.imgflip.com/lg8my4.jpg",
+				"width": 600,
+				"height": 908,
+				"box_count": 3
+			},
+			{
+				"id": "14371066",
+				"name": "Star Wars Yoda",
+				"url": "https://i.imgflip.com/8k0sa.jpg",
+				"width": 620,
+				"height": 714,
+				"box_count": 2
+			},
+			{
+				"id": "61544",
+				"name": "Success Kid",
+				"url": "https://i.imgflip.com/lbhk.jpg",
+				"width": 500,
+				"height": 500,
+				"box_count": 2
+			},
+			{
+				"id": "252600902",
+				"name": "Always Has Been",
+				"url": "https://i.imgflip.com/46e43q.jpg",
+				"width": 960,
+				"height": 540,
+				"box_count": 2
+			},
+			{
+				"id": "226297822",
+				"name": "Panik Kalm Panik",
+				"url": "https://i.imgflip.com/3qqcim.jpg",
+				"width": 640,
+				"height": 881,
+				"box_count": 3
+			},
+			{
+				"id": "178591752",
+				"name": "Tuxedo Winnie The Pooh",
+				"url": "https://i.imgflip.com/2ybua0.jpg",
+				"width": 800,
+				"height": 582,
+				"box_count": 2
+			}
+		]
+	}
+}
