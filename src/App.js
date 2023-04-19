8636abd0a44f411ec8870f41353a7a60dbfb7b2b
leslieshea <lshea@audigy.com>
2023-04-19 11:59:36 -0700

    Created Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+      <h1>Hello World</h1>
+    </div>
   );
 }
